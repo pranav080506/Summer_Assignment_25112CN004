04ae8e5ca7366872dc76ac90e23f392017fbb736
pranav080506 <pranavgupta080506@gmail.com>
2026-05-29 12:28:47 +0530

    day 1 progress

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+    for(int i; i<=10; i++){
+        cout << n <<"x" << i <<"="<< n*i <<endl;
+    }
+    return 0;
+
+
+}
